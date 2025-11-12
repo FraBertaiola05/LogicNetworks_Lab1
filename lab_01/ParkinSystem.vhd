db173f24d9b76d76fc2a1cbe2d0aa6155c6a29b0
steGiro05 <stefanogiroletti9@gmail.com>
2025-11-12 15:00:47 +0100

    updated names

--- lab_01/ParkinSystem.vhd
+++ lab_01/ParkinSystem.vhd
@@ -19,14 +19,27 @@ end Car_Parking_System_VHDL;
 
 architecture arch of Car_Parking_System_VHDL is
     --State declarations
-    type parking_state is (Idle, Wait_pass, Pass_correct, Stop, Wrong_pass, Right_pass);
-    signal State : parking_state;
+    type FSM_States is (IDLE, WAIT_PASSWORD, WRONG_PASS, RIGHT_PASS, STOP);
+    signal State : FSM_States;
     --Counter
     signal Counter : integer range 0 to ClockFrequencyHz * 60;
 
 
 begin
     process(clk) is
+
+        -- Procedure for changing state after a given time
+        procedure ChangeState(ToState : FSM_States;
+                              Seconds : integer := 0) is
+            variable ClockCycles  : integer;
+        begin
+            ClockCycles  := Seconds * ClockFrequencyHz -1;
+            if Counter = ClockCycles then
+                Counter <= 0;
+                State   <= ToState;
+            end if;
+        end procedure;
+
     begin
     end process;
 
